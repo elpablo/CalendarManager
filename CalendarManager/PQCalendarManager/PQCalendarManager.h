dfd11c27194b6b107f87d2df6a9a6ddba72a6dcc
Paolo Quadrani <paolo.quadrani@gmail.com>
2012-08-06 16:02:34 +0200

    Added documentation.

--- CalendarManager/PQCalendarManager/PQCalendarManager.h
+++ CalendarManager/PQCalendarManager/PQCalendarManager.h
@@ -34,15 +34,28 @@
 
 /////////////////////// Calendar API ///////////////////////
 
-- (BOOL)iCloudCalendarIsPresent;
-- (void)setDefaultCalendarWithName:(NSString *)calName;
-
+/// Returns the array of calendar source given the source type (EKSourceTypeLocal, ...)
 - (NSArray *)calendarSourcesOfType:(EKSourceType)type;
+
+/// Commodity method that returns the array of local calendars.
 - (NSArray *)localCalendars;
+
+/// Commodity method that return the array of CalDAV calendars.
 - (NSArray *)calDavCalendars;
+
+/// Commodity method that return the array of birthdays calendars.
 - (NSArray *)birthdaysCalendars;
 
+/// Allows to set a default calendar with a given name.
+- (void)setDefaultCalendarWithName:(NSString *)calName;
+
+/// Check the existance of any iCloud calendar.
+- (BOOL)iCloudCalendarIsPresent;
+
+/// Allows to add a new calendar to the given calendar source. The created calendar is returned if no errors occourred nil otherwise.
 - (EKCalendar *)addCalendarWithSource:(EKSource *)source name:(NSString *)calName color:(UIColor *)c makeDefault:(BOOL)def error:(NSError **)error;
+
+/// Allows to remove the given calendar.
 - (BOOL)removeCalendar:(EKCalendar *)calendar error:(NSError **)error;
 
 
@@ -50,11 +63,28 @@
 
 /// Show the events for today from the given calendar. Pass nil to fetch events from default calendar.
 - (NSArray *)eventsForTodayInCalendar:(EKCalendar *)cal;
+
 /// Show the events for current month from the given calendar. Pass nil to fetch events from default calendar.
 - (NSArray *)eventsForCurrentMonthInCalendar:(EKCalendar *)cal;
+
+/// fetch all the events between the startDate and endDate associated to the given calendar.
 - (NSArray *)eventsForCalendar:(EKCalendar *)cal fromDate:(NSDate *)startDate toDate:(NSDate *)endDate;
+
+/// Allows to create a new event with given parameters.
+/**
+ calendarManager:didCreateEvent: and calendarManager:needToPresentController: will be called after that the new event has been created to inform 
+ the delegate that the event has been created and that the edit event view controller should be presented to the user.
+ */
 - (void)addEventToCalendar:(EKCalendar *)cal withTitle:(NSString *)t location:(NSString *)loc startDate:(NSDate *)start endDate:(NSDate *)end description:(NSString *)note;
+
+/// This method allows to save the created event.
+/** 
+ This method is called when the used save the event from the edit event view controller presented from the addEventToCalendar method or can be called by the user
+ is the controller has not been presented.
+ */
 - (BOOL)saveEvent:(EKEvent *)ev error:(NSError **)error;
+
+/// This method allows to create an alarm with the given time duration in minutes. Then it can be passed to a created event.
 - (EKAlarm *)createAlarmOfMinutes:(NSUInteger)min;
 
 @end
